9a1798fd4605c4495c931516491c4899fa3310bd
Robinson Lopez <robins.616009@gmail.com>
2024-01-20 18:59:44 -0500

    chore: added features component

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,11 +1,15 @@
-import { Hero } from '@/components/Home/Hero'
-import Image from 'next/image'
+import { Features } from "@/components/Home/Features";
+import { Hero } from "@/components/Home/Hero";
+import { Container } from "@mantine/core";
+import Image from "next/image";
 
 export default function Home() {
   return (
     <main>
-      <Hero/>
-
+      <Hero />
+      <Container>
+        <Features />
+      </Container>
     </main>
-  )
+  );
 }
